0687d74be22b7edaeb2bacfe634e13602d36c2c7
elcorremachitos <elcorremachitos.2015@gmail.com>
2026-07-20 19:18:30 -0500

    feat: add standard API response wrapper and error body

---
+++ src/main/java/com/chamo/chamotech/dto/ApiResponse.java
@@ -0,0 +1,28 @@
+package com.chamo.chamotech.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        ApiResponse<T> response = new ApiResponse<>();
+        response.setSuccess(true);
+        response.setMessage(message);
+        response.setData(data);
+        return response;
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        ApiResponse<T> response = new ApiResponse<>();
+        response.setSuccess(false);
+        response.setMessage(message);
+        return response;
+    }
+}
---
+++ src/main/java/com/chamo/chamotech/dto/ErrorResponseDTO.java
@@ -0,0 +1,18 @@
+package com.chamo.chamotech.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponseDTO {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String message;
+    private String path;
+}
