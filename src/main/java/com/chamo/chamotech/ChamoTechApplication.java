e37041d49178a3e3b3ccf16ae4099b85eae5c31a
elcorremachitos <elcorremachitos.2015@gmail.com>
2026-07-20 19:18:22 -0500

    chore: scaffold Spring Boot project with H2/MySQL profiles and Swagger

---
+++ src/main/java/com/chamo/chamotech/ChamoTechApplication.java
@@ -0,0 +1,13 @@
+package com.chamo.chamotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamoTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChamoTechApplication.class, args);
+    }
+
+}
