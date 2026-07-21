b8f9497c272cfb1524ef233cbed4f17a48c5e704
elcorremachitos <elcorremachitos.2015@gmail.com>
2026-07-20 19:19:11 -0500

    feat: register JWT filter in the servlet filter chain

---
+++ src/main/java/com/chamo/chamotech/config/FilterConfig.java
@@ -0,0 +1,19 @@
+package com.chamo.chamotech.config;
+
+import com.chamo.chamotech.security.JwtAuthFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    FilterRegistrationBean<JwtAuthFilter> jwtFilter(JwtAuthFilter jwtAuthFilter) {
+        FilterRegistrationBean<JwtAuthFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(jwtAuthFilter);
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
+}
