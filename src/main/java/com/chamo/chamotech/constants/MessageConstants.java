17f63bdd0b1a46bf8d47785e30b579faae538711
elcorremachitos <elcorremachitos.2015@gmail.com>
2026-07-20 19:18:31 -0500

    feat: add centralized message constants

---
+++ src/main/java/com/chamo/chamotech/constants/MessageConstants.java
@@ -0,0 +1,47 @@
+package com.chamo.chamotech.constants;
+
+public class MessageConstants {
+
+    public static String CUSTOMER_NOT_FOUND = "CUSTOMER NOT FOUND";
+    public static String CUSTOMER_FOUND = "CUSTOMER FOUND";
+    public static String CUSTOMER_LIST = "CUSTOMER LIST";
+    public static String CUSTOMER_CREATED = "CUSTOMER CREATED";
+    public static String CUSTOMER_UPDATED = "CUSTOMER UPDATED";
+    public static String CUSTOMER_DELETED = "CUSTOMER DELETED";
+    public static String CUSTOMER_EMAIL_EXISTS = "A CUSTOMER WITH THIS EMAIL ALREADY EXISTS";
+
+    public static String CATEGORY_NOT_FOUND = "CATEGORY NOT FOUND";
+    public static String CATEGORY_FOUND = "CATEGORY FOUND";
+    public static String CATEGORY_LIST = "CATEGORY LIST";
+    public static String CATEGORY_CREATED = "CATEGORY CREATED";
+    public static String CATEGORY_UPDATED = "CATEGORY UPDATED";
+    public static String CATEGORY_DELETED = "CATEGORY DELETED";
+    public static String CATEGORY_NAME_EXISTS = "A CATEGORY WITH THIS NAME ALREADY EXISTS";
+    public static String CATEGORY_HAS_PRODUCTS = "CANNOT DELETE A CATEGORY WITH PRODUCTS ASSOCIATED";
+
+    public static String TAG_NOT_FOUND = "TAG NOT FOUND";
+    public static String TAG_FOUND = "TAG FOUND";
+    public static String TAG_LIST = "TAG LIST";
+    public static String TAG_CREATED = "TAG CREATED";
+    public static String TAG_UPDATED = "TAG UPDATED";
+    public static String TAG_DELETED = "TAG DELETED";
+    public static String TAG_NAME_EXISTS = "A TAG WITH THIS NAME ALREADY EXISTS";
+
+    public static String PRODUCT_NOT_FOUND = "PRODUCT NOT FOUND";
+    public static String PRODUCT_FOUND = "PRODUCT FOUND";
+    public static String PRODUCT_LIST = "PRODUCT LIST";
+    public static String PRODUCT_CREATED = "PRODUCT CREATED";
+    public static String PRODUCT_UPDATED = "PRODUCT UPDATED";
+    public static String PRODUCT_DELETED = "PRODUCT DELETED";
+    public static String PRODUCT_IN_ORDERS = "CANNOT DELETE A PRODUCT THAT IS ALREADY PART OF AN ORDER";
+    public static String PRODUCT_OUT_OF_STOCK = "NOT ENOUGH STOCK FOR PRODUCT: ";
+
+    public static String ORDER_NOT_FOUND = "ORDER NOT FOUND";
+    public static String ORDER_FOUND = "ORDER FOUND";
+    public static String ORDER_LIST = "ORDER LIST";
+    public static String ORDER_CREATED = "ORDER CREATED SUCCESSFULLY";
+
+    public static String USERNAME_TAKEN = "USERNAME ALREADY TAKEN";
+    public static String INVALID_CREDENTIALS = "INVALID USERNAME OR PASSWORD";
+    public static String USER_REGISTERED = "USER REGISTERED SUCCESSFULLY";
+}
